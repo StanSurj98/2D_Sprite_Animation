944de26d05676f0d2dfe3d83b5b75ef10e48dd6b
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-11 15:48:51 -0700

    get each sprite frame on canvas

--- public/script.js
+++ public/script.js
@@ -11,15 +11,21 @@ const CANVAS_HEIGHT = (canvas.height = 600);
 const playerImg = new Image(); // new instance of Image is just like <img /> provide src=""
 playerImg.src = "./images/shadow_dog_frames.png";
 
+// Sprite Dimensions - Taken by dividing total size of png, divide by the columns & rows for each individual frame
+const SPRITE_WIDTH = 575; // 6876 / 11 columns
+const SPRITE_HEIGHT = 523; // 5230 / 10 rows
+
+
 // Animation Loop
 const animate = () => {
   // beginning of each loop, clear canvas
-  // ctx.clearRect(x-start, y-start, w, h) -> 0, 0 == top left to entire width && height
+  // .clearRect(x-start, y-start, w, h) -> 0, 0 == top left to entire width && height
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
   // Passing in the animation frames, drawing from top-left(0, 0)
-  // .drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh) | s = src, d = dest
-  ctx.drawImage(playerImg, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // .drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh) | s = src, d = dest | dest is WHERE we place the cutout src
+  ctx.drawImage(playerImg, 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // Cutting out from top-left to each sprite frame, where is it appearing? Top-left to entire canvas width/height
 
   // This is built-in method for a recursion loop
   requestAnimationFrame(animate);
