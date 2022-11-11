ee519ccf0f148d03ee6dddcdfb7909793fde7f49
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-11 12:39:42 -0700

    messing around with canvas

--- public/script.js
+++ public/script.js
@@ -1,2 +1,7 @@
 const canvas = document.getElementById("canvas-1");
+// Define the 2d context, with ctx, we can now draw anything inside
 const ctx = canvas.getContext("2d");
+// Drawing a Rectangle - ctx is now an object with several draw properties
+console.log(ctx);
+ctx.fillStyle = "cyan"
+ctx.fillRect(0, 0, 100, 150);
