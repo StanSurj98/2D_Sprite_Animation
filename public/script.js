c1a5973d71716466cb03b59402b136366ad33e43
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-11 12:43:13 -0700

    set dimensions properly and drew rectangle

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 const canvas = document.getElementById("canvas-1");
 // Define the 2d context, with ctx, we can now draw anything inside
 const ctx = canvas.getContext("2d");
-// Drawing a Rectangle - ctx is now an object with several draw properties
-console.log(ctx);
-ctx.fillStyle = "cyan"
-ctx.fillRect(0, 0, 100, 150);
+// Set its dimensions same as style.css otherwise it won't scale right, defaults 150x300
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+ctx.fillStyle = "cyan";
+ctx.fillRect(0, 0, 150, 100);
+
+// Bringing an image into the canvas
