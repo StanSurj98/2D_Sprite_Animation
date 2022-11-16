c8f0c56e1b89c45e9d0ed64d07a92c30dbf415d5
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-15 22:46:16 -0700

    add simple animation logic and navigating frames, staggerFrames to slow down

--- public/script.js
+++ public/script.js
@@ -13,7 +13,11 @@ playerImg.src = "./images/shadow_dog_frames.png";
 // Sprite Dimensions - Taken by dividing total size of png, divide by the columns & rows for each individual frame
 const SPRITE_WIDTH = 575; // 6876 / 11 columns
 const SPRITE_HEIGHT = 523; // 5230 / 10 rows
-
+// Frame Controllers
+let frameX = 0; // each num advances the src cutout to one frame on x or y axis
+let frameY = 4; // x = each frame in same action, y = different actions for the fox
+let gameFrame = 0; // this will help slow down the animation
+const staggerFrames = 2 // this is DIRECTLY the amount of frames it slows down by
 
 // ----- Animation Loop -----
 const animate = () => {
@@ -23,8 +27,24 @@ const animate = () => {
 
   // "Selecting" EACH sprite frame and then drawing in from top-left(0, 0) of canvas to its intended size
   // .drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh) | s = src, d = dest | dest is WHERE we place the cutout src
-  ctx.drawImage(playerImg, 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT, 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT);
+  ctx.drawImage(
+    playerImg,
+    frameX * SPRITE_WIDTH,
+    frameY * SPRITE_HEIGHT,
+    SPRITE_WIDTH,
+    SPRITE_HEIGHT,
+    0,
+    0,
+    SPRITE_WIDTH,
+    SPRITE_HEIGHT
+  );
 
+  if (gameFrame % staggerFrames === 0) { // For every staggerFrames frames increment frameX
+    if (frameX < 9) frameX++;
+    else frameX = 0;
+  }
+
+  gameFrame++;
   // This is built-in method for a recursion loop
   requestAnimationFrame(animate);
 };
